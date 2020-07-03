f9c0d9aedf699c072600115301cb7303ee704f8d
Ankit <plnkt61@gmail.com>
2020-07-03 15:49:53 +0530

    Cards created and Components proptyped

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React,{Component} from 'react';
+import Cards from './components/Cards/Cards'
+import CountryPicker from './components/CountryPicker/CountryPicker'
+import Charts from './components/Charts/Charts'
+import styles from './App.module.css'
+import { fetchData } from './api'
+
 
 
 class App extends Component{
+
+    state = {
+        data :{}
+    }
+
+    async componentDidMount() {
+        
+        const fetchedData = await fetchData();
+
+        this.setState({ data : fetchedData })
+        console.log(fetchedData)
+    }
+
     render(){
+
+        const { data } = this.state
+
         return(
-            <div><h1>Covidtracker</h1></div>
+            <div className={styles.container}>
+                <Cards data={ data } />
+                <CountryPicker/>
+                <Charts/>
+            </div>
         )
     }
 }
